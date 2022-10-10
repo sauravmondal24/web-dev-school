6d5a60388e76b9a4a8162200e20dbdb9583e8378
sauravmondal24 <sauravmondal321@gmail.com>
2022-10-10 22:50:14 +0600

    Banner section added

--- src/components/Header/Banner.js
+++ src/components/Header/Banner.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import './Banner.css';
 
-const Banner = () => {
+function Banner() {
 	return (
-		<div>
-			<h3>This is banner page</h3>
-		</div>
+		<Carousel fade>
+			<Carousel.Item className="bannerPic">
+				<img
+					className="d-block w-100 m-auto bannerPic"
+					src="https://intelog.net/wp-content/uploads/2019/05/Web-Development0.jpg"
+					alt="First slide"
+				/>
+				<Carousel.Caption>
+					<h2>A JavaScript library for building user interfaces</h2>
+					<p>
+						React · Declarative. React makes it painless to create interactive
+						UIs. · Component-Based. Build encapsulated components that manage
+						their own state, then compose
+					</p>
+				</Carousel.Caption>
+			</Carousel.Item>
+			<Carousel.Item className="bannerPic">
+				<img
+					className="d-block w-100 "
+					src="https://ichef.bbci.co.uk/news/400/cpsprodpb/A1FA/production/_105966414_gettyimages-871919258.jpg"
+					alt="Second slide"
+				/>
+
+				<Carousel.Caption>
+					<h2>JavaScript is the world's most popular programming language</h2>
+					<p>
+						JavaScript is a scripting language that enables you to create
+						dynamically updating content, control multimedia, animate images,
+						and pretty much everything else.
+					</p>
+				</Carousel.Caption>
+			</Carousel.Item>
+			<Carousel.Item className="bannerPic">
+				<img
+					className="d-block w-100 bannerPic"
+					src="https://talktobusiness.com/wp-content/uploads/2022/04/What-Is-Web-Development.jpg"
+					alt="Third slide"
+				/>
+
+				<Carousel.Caption>
+					<h3>CSS stands for Cascading Style Sheets</h3>
+					<p>
+						Cascading Style Sheets (CSS) is a stylesheet language used to
+						describe the presentation of a document written in HTML or XML
+						(including XML dialects such as SVG, MathML or XHTML).
+					</p>
+				</Carousel.Caption>
+			</Carousel.Item>
+		</Carousel>
 	);
-};
+}
 
 export default Banner;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,53 +1,57 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Banner from './Banner';
 
 const Header = () => {
 	return (
-		<nav className="navbar navbar-expand-lg bg-light">
-			<div className="container">
-				<button
-					className="navbar-toggler"
-					type="button"
-					data-bs-toggle="collapse"
-					data-bs-target="#navbarTogglerDemo03"
-					aria-controls="navbarTogglerDemo03"
-					aria-expanded="false"
-					aria-label="Toggle navigation"
-				>
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<Link className="navbar-brand fw-bolder" to="/">
-					WEB DEV SCHOOL
-				</Link>
-				<div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-					<ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-						<li className="nav-item">
-							<Link
-								to="/"
-								className="nav-link active"
-								aria-current="page"
-								href="#"
-							></Link>
-						</li>
-						<li className="nav-item">
-							<Link to="/topics" className="nav-link fw-bold" href="#">
-								Topics
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link to="/statistics" className="nav-link fw-bold" href="#">
-								Statistics
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link to="/blog" className="nav-link fw-bold" href="#">
-								Blog
-							</Link>
-						</li>
-					</ul>
+		<div>
+			<nav className="navbar navbar-expand-lg py-3 bg-light">
+				<div className="container">
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarTogglerDemo03"
+						aria-controls="navbarTogglerDemo03"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+					>
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<Link className="navbar-brand fw-bolder" to="/">
+						WEB DEV SCHOOL
+					</Link>
+					<div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+						<ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+							<li className="nav-item">
+								<Link
+									to="/"
+									className="nav-link active"
+									aria-current="page"
+									href="#"
+								></Link>
+							</li>
+							<li className="nav-item">
+								<Link to="/topics" className="nav-link fw-bold" href="#">
+									Topics
+								</Link>
+							</li>
+							<li className="nav-item">
+								<Link to="/statistics" className="nav-link fw-bold" href="#">
+									Statistics
+								</Link>
+							</li>
+							<li className="nav-item">
+								<Link to="/blog" className="nav-link fw-bold" href="#">
+									Blog
+								</Link>
+							</li>
+						</ul>
+					</div>
 				</div>
-			</div>
-		</nav>
+			</nav>
+			<Banner></Banner>
+		</div>
 
 		// <nav>
 		// 	<Link to="/"></Link>
